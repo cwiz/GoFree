8640f2278bce84cca103d45678d025a89de27a19
Sergei Surovtsev <cyberwizard.ru@gmail.com>
2013-03-13 03:49:04 +0400

    hotels

---
+++ app/server/api/hotels.js
@@ -0,0 +1,36 @@
+(function(){
+  var geobase;
+  geobase = require("./../geobase");
+  exports.details = function(req, res){
+    var provider, id, error;
+    provider = req.params.provider;
+    id = +req.params.id;
+    if (!provider || !id) {
+      return res.json({
+        status: 'error',
+        message: 'Please supply provider or id'
+      });
+    }
+    try {
+      provider = require("./providers/" + provider);
+    } catch (e$) {
+      error = e$;
+      return res.json({
+        status: 'error',
+        message: 'No such provider'
+      });
+    }
+    return provider.details(id, function(error, hotel){
+      if (error || !hotel) {
+        return res.json({
+          status: 'error',
+          message: error
+        });
+      }
+      return res.json({
+        status: 'ok',
+        hotel: hotel
+      });
+    });
+  };
+}).call(this);
